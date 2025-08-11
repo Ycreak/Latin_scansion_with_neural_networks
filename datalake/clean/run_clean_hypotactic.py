6b5e413771bbb95cff35a91a5b08a7c4ef5e06cf
Luuk Nolden <luuk.nolden@naturalis.nl>
2025-08-11 10:38:48 +0200

    Fixes to the datalake

--- datalake/clean/run_clean_hypotactic.py
+++ datalake/clean/run_clean_hypotactic.py
@@ -3,7 +3,7 @@ from datalake.clean.clean_hypotactic import Hypotactic
 def run():
     source_name = "hypotactic"
     
-    Hypotactic().run(source_path=f"lsnn/bucket/raw/{source_name}", destination_path=f"lsnn/bucket/clean/{source_name}")
+    Hypotactic().run(source_path=f"datalake/bucket/raw/{source_name}", destination_path=f"datalake/bucket/clean/{source_name}")
 
 
 if __name__ == "__main__":
--- datalake/clean/run_clean_pedecerto.py
+++ datalake/clean/run_clean_pedecerto.py
@@ -3,7 +3,7 @@ from datalake.clean.clean_pedecerto import Pedecerto
 def run():
     source_name = "pedecerto"
     
-    Pedecerto().run(source_path=f"lsnn/bucket/raw/{source_name}", destination_path=f"lsnn/bucket/clean/{source_name}")
+    Pedecerto().run(source_path=f"datalake/bucket/raw/{source_name}", destination_path=f"datalake/bucket/clean/{source_name}")
 
 
 if __name__ == "__main__":
